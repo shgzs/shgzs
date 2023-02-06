198b7b767b114f86048d3fda0d15b33ce0f59c3f
Hadi Ghasemi <dr.ghasemi@gmail.com>
2023-02-06 23:01:06 +0330

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';    
+import { AfterContentInit, AfterViewInit, DoCheck, NgModule, OnChanges, OnInit } from '@angular/core';    
+import {FormsModule,FormControl} from '@angular/forms'; 
+import {Observable} from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
+import { MatAutocomplete } from '@angular/material';
+ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterViewInit {
+  twoWay="";
+  log:string[]=[];
+
+  ngAfterViewInit(){
+    this.log.push('ngAfterViewInit');
+  }
+  ngAfterContentInit(){
+    this.log.push('ngAfterContentInit');
+  }
+  ngDoCheck(){
+    this.log.push('ngDoCheck');
+  }
+  ngOnChanges(){
+    this.log.push('ngOnChanges');
+  }
+  ngOnInit(): void {
+    this.log.push("start")
+  }
   title = 'my-app';
+
+  name="ali";
 }
